34d252b3f91f457af311f68c3a2a856c5e0b7af9
Stephen Keller <stephen.keller@cisecurity.org>
2019-02-09 16:47:32 -0500

    Fixed issue when a syntax issue exists within the tests

--- src/FancyTest.php
+++ src/FancyTest.php
@@ -3,13 +3,12 @@
 namespace stekel\AutoTest;
 
 use stekel\AutoTest\Commands\PhpUnit;
-use stekel\AutoTest\Config;
-use stekel\AutoTest\Str;
 
 /**
  * FancyTest Class
  */
-class FancyTest {
+class FancyTest
+{
     
     /**
      * Command
@@ -28,8 +27,8 @@ class FancyTest {
     /**
      * Construct
      */
-    public function __construct(PhpUnit $command, Config $config) {
-        
+    public function __construct(PhpUnit $command, Config $config)
+    {
         $this->command = $command;
         $this->config = $config;
     }
@@ -39,76 +38,65 @@ class FancyTest {
      *
      * @return void
      */
-    public function fire() {
-        
+    public function fire()
+    {
         $handle = $this->command->execute();
         $phpunitDone = false;
         
         if ($this->config->removePhpUnitHeader()) {
-            
             $read = fgets($handle);
             
-            if (! Str::startsWith($read, 'PHPUnit')) {
+            if ($read) {
+                if (! Str::startsWith($read, 'PHPUnit')) {
+                    rewind($handle);
+                }
                 
-                rewind($handle);
-            }
-            
-            $read = fgets($handle);
-            
-            if (! Str::startsWith($read, "\n")) {
+                $read = fgets($handle);
                 
-                rewind($handle);
+                if ($read) {
+                    if (! Str::startsWith($read, "\n")) {
+                        rewind($handle);
+                    }
+                }
             }
         }
         
         while (!feof($handle)) {
-            
             if ($phpunitDone) {
-                
                 $read = fgets($handle);
             } else {
-                
                 $read = fread($handle, 128);
             }
             
             if (! Str::contains($read, '.')) {
-                
                 $phpunitDone = true;
             }
             
             $output = $read;
             
             if ($this->config->simplifyLaravelPipeline()) {
-                
                 if (Str::contains($output, 'vendor/laravel')) {
-                    
                     echo 'Laravel framework pipline: ';
                     
                     while (true) {
-                        
                         $read = fgets($handle);
                         
                         if (Str::contains($read, 'vendor/laravel')) {
-                            
                             echo '.';
                         } else {
-                            
                             echo "\n";
                             $output = $read;
                             break;
                         }
                     }
                 }
-                
             }
             
-            if ($this->config->simplifyProjectPath() && ! Str::startsWith($output, '-') && ! Str::startsWith($output, '+') ) {
-                
+            if ($this->config->simplifyProjectPath() && ! Str::startsWith($output, '-') && ! Str::startsWith($output, '+')) {
                 $output = str_replace(__DIR__, '{project}', $output);
             }
             
             if ($output == "ERRORS!\n" || $output == "FAILURES!\n") {
-                
                 fclose($handle);
                 
                 return -1;
